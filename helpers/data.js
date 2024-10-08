c176ebb28ce43bafdd61e158cad6eb7813ebbfd5
Aszton <bartoszkuczera88@gmail.com>
2024-10-08 13:36:30 +0200

    duze_zmiany

---
+++ helpers/data.js
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+export const baseUrl = "https://demoqa.com"
+export const userID = "b97bd7d2-1c59-4b3e-97db-f8cc4d3561d5"
+export const user = "bartosz_k"
+export const secretPassword = process.env.SECRET_PASSWORD
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,10 +1,69 @@
 import pkg from "pactum";
 const { spec } = pkg;
 import { expect } from "chai";
+import { baseUrl, userID, user, secretPassword } from "../helpers/data.js";
+let token;
 
 describe("API tests", () => {
-  it("first test", async () => {
-    const response = await spec().get("https://demoqa.com/BookStore/v1/Books");
+  it.skip("first test", async () => {
+    const response = await spec().get(`${baseUrl}/BookStore/v1/Books`);
     expect(response.statusCode).to.eql(200);
+    expect(response.body.books[0].title).to.eql("Git Pocket Guide");
+  });
+
+  it.skip("create account", async () => {
+    const response = await spec().post(`${baseUrl}/Account/v1/User`).withBody({
+      userName: user,
+      password: secretPassword,
+    });
+    expect(response.statusCode).to.eql(201);
+  });
+
+  it("Generate token", async () => {
+    const response = await spec()
+      .post(`${baseUrl}/Account/v1/GenerateToken`)
+      .withBody({
+        userName: user,
+        password: secretPassword,
+      });
+    token = response.body.token;
+    expect(response.statusCode).to.eql(200);
+  });
+
+  it("Get user ID", async () => {
+    const response = await spec()
+      .get(`${baseUrl}/Account/v1/User/${userID}`)
+      .withBearerToken(token);
+    expect(response.statusCode).to.eql(200);
+  });
+
+  it("Add a book", async () => {
+    const response = await spec()
+    .post(`${baseUrl}/BookStore/v1/Books`)
+    .withBearerToken(token)
+    .withBody({
+        "userId": userID,
+        "collectionOfIsbns": [
+          {
+            "isbn": "9781449325862"
+          }
+        ]
+      })
+      expect(response.statusCode).to.eql(201)
+  })
+
+  it("Delete all books from user 'Kuczer'", async () => {
+    const response = await spec()
+      .delete(`${baseUrl}/BookStore/v1/Books?UserId=${userID}`)
+      .withBearerToken(token);
+    expect(response.statusCode).to.eql(204);
+  });
+
+  it("Get User 'Kuczer' and assert empty books list", async () => {
+    const response = await spec()
+      .get(`${baseUrl}/Account/v1/User/${userID}`)
+      .withBearerToken(token);
+    expect(response.statusCode).to.eql(200);
+    expect(response.body.books).to.eql([]);
   });
 });
